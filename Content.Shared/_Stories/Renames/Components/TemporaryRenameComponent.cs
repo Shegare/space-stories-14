24eac3c0433fea4716d1f48d8a44159b147f78fb
Shegare <228shadowfox228@gmail.com>
2025-07-13 21:58:25 +0300

    TemporaryRename

---
+++ Content.Shared/_Stories/Renames/Components/TemporaryRenameComponent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._Stories.Renames;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(TemporaryRenameSystem))]
+public sealed partial class TemporaryRenameComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public string? CurrentName { get; set; }
+
+    [DataField, AutoNetworkedField]
+    public float LifeTime = 10f;
+}
---
+++ Content.Shared/_Stories/Renames/EntitySystems/TemporaryRenameSystem.cs
@@ -0,0 +1,61 @@
+﻿using Content.Shared.NameModifier.EntitySystems;
+
+namespace Content.Shared._Stories.Renames;
+
+public sealed class TemporaryRenameSystem : EntitySystem
+{
+    [Dependency] private readonly NameModifierSystem _nameModifier = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<TemporaryRenameComponent, MapInitEvent>(OnLabelCompMapInit);
+        SubscribeLocalEvent<TemporaryRenameComponent, RefreshNameModifiersEvent>(OnRefreshNameModifiers);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<TemporaryRenameComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (comp.LifeTime <= 0)
+            {
+                comp.CurrentName = null;
+                _nameModifier.RefreshNameModifiers(uid);
+                RemComp<TemporaryRenameComponent>(uid);
+            }
+            else
+                comp.LifeTime -= frameTime;
+        }
+    }
+
+    private void OnLabelCompMapInit(Entity<TemporaryRenameComponent> ent, ref MapInitEvent args)
+    {
+        if (!string.IsNullOrEmpty(ent.Comp.CurrentName))
+        {
+            ent.Comp.CurrentName = Loc.GetString(ent.Comp.CurrentName);
+            Dirty(ent);
+        }
+
+        _nameModifier.RefreshNameModifiers(ent.Owner);
+    }
+
+    public void Rename(EntityUid uid, string? text, MetaDataComponent? metadata = null, TemporaryRenameComponent? rename = null)
+    {
+        rename ??= EnsureComp<TemporaryRenameComponent>(uid);
+
+        rename.CurrentName = text;
+        _nameModifier.RefreshNameModifiers(uid);
+
+        Dirty(uid, rename);
+    }
+
+    private void OnRefreshNameModifiers(Entity<TemporaryRenameComponent> entity, ref RefreshNameModifiersEvent args)
+    {
+        if (!string.IsNullOrEmpty(entity.Comp.CurrentName))
+            args.AddModifier(entity.Comp.CurrentName);
+    }
+}
